92a930a4d7b9b801e1306deae224febaa4001438
Sangnn <sangnnph14292@fpt.edu.vn>
2023-11-20 10:05:01 +0700

    update

--- app/home/[id]/Description/page.jsx
+++ app/home/[id]/Description/page.jsx
@@ -1,14 +1,26 @@
-
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '@/app/styles/home.module.css';
 
 const Description = ({ description }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const maxLength = 150;
+
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
     <div className={styles.containerdescription}>
       <div className={styles.inline}></div>
       <div className={styles.description}>
         <p>
-          <strong>Mô tả:</strong> {description}
+          <strong>Mô tả:</strong>{' '}
+          {isExpanded ? description : `${description.slice(0, maxLength)}${description.length > maxLength ? '...' : ''}`}
+          {description.length > maxLength && (
+            <span onClick={toggleExpand} style={{ cursor: 'pointer', color: 'blue', marginLeft: '5px' }}>
+              {isExpanded ? 'Thu gọn' : 'Xem thêm'}
+            </span>
+          )}
         </p>
       </div>
     </div>
